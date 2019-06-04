fc070c8d8bb1aeedf954e41ade79cc9c9c5399b1
Sander Radionov <sanderradionov@hotmail.com>
2019-06-04 12:39:22 +0300

    Loo klass "Kitarr".

---
+++ Lavendieksam_RadionovTARpe17/Models/Kitarr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lavendieksam_RadionovTARpe17.Models
+{
+	public class Kitarr
+	{
+		public int Id { get; set; }
+		public string Mudel { get; set; }
+		public int Hind { get; set; }
+
+	}
+}
