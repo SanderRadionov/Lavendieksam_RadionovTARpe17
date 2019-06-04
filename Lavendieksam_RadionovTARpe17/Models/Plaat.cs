cdb680c806aa32fedcd21c6bbcd699538b0abce4
Sander Radionov <sanderradionov@hotmail.com>
2019-06-04 12:40:44 +0300

    Loo klass "Plaat".

---
+++ Lavendieksam_RadionovTARpe17/Models/Plaat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lavendieksam_RadionovTARpe17.Models
+{
+	public class Plaat
+	{
+		public int Id { get; set; }
+		public string Nimi { get; set; }
+		public string Artist { get; set; }
+		public string Žanr { get; set; }
+		public int Hind { get; set; }
+	}
+}
